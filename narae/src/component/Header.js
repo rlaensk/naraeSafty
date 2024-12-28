a2fa16f7dd956b0d237d1f87e2618c9639d267da
김두나 <110874398+rlaensk@users.noreply.github.com>
2024-12-29 02:50:59 +0900

    29

---
+++ narae/src/component/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <div className="navBox">
+                <div className="logo">
+
+                </div>
+                <nav>
+                    <ul>
+                        <li>회사소개</li>
+                        <li>제품정보</li>
+                        <li>사업분야</li>
+                        <li>고객센터</li>
+                        <li>쇼핑몰</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+
+
+}
+export default Header;
